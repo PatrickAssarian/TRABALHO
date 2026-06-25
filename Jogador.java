0bfe7fd1eb044cf147aef623aa428951765a1e06
PatrickAssarian <patrickassarian@gmail.com>
2026-06-25 13:11:06 -0300

    ADIÇÃO DE MÉTODOS

--- Jogador.java
+++ Jogador.java
@@ -1,8 +1,16 @@
 public class Jogador extends Personagem {
     private int percepcao;
+    private boolean temArma;
+    private int  municao;
+    private int kitsMedicos;
+    private boolean temBastao;
     public Jogador(int percepcao, int saude, int posicaox, int posicaoy) {
         super(5, posicaox, posicaoy);
         this.percepcao = percepcao;
+        this.temArma = false;
+        this.municao = 0;
+        this.kitsMedicos = 0;
+        this.temBastao = false;
     }
 
     public int getPercepcao() {
@@ -10,5 +18,41 @@ public class Jogador extends Personagem {
     }
     public void setPercepcao(int percepcao) {
         this.percepcao = percepcao;
+    }
+    public boolean isTemArma() { 
+        return temArma;
+    }
+    public void setTemArma(boolean temArma) { 
+        this.temArma = temArma; 
+    }
+    public int getMunicao() { 
+        return municao; 
+    }
+    public void setMunicao(int municao) { 
+        this.municao = municao; 
+    }
+    public int getKitsMedicos() { 
+        return kitsMedicos; 
+    }
+    public void setKitsMedicos(int kitsMedicos) { 
+        this.kitsMedicos = kitsMedicos; 
+    }
+    public boolean isTemBastao() { 
+        return temBastao; 
+    }
+    public void setTemBastao(boolean temBastao) { 
+        this.temBastao = temBastao; 
+    }
+    public void exibirStatusJogador(Jogador jogador) {
+        IO.println("\n--- STATUS DO SOBREVIVENTE ---");
+        IO.println("Vida: " + jogador.getSaude());
+    
+        if (jogador.isTemArma()) {
+           IO.println("Arma de Dardos: [ EQUIPADA ]");
+          IO.println("Munição: " + jogador.getMunicao() + " dardo(s)");
+        } else {
+            IO.println("Arma: [ VAZIO ]");
+        }
+    IO.println("------------------------------\n");
     }
 }
--- Mapa.java
+++ Mapa.java
@@ -175,40 +175,286 @@ public class Mapa {
         grade[posicaoTx][posicaoTy].setDinossauro(tiranossauroRex);
     }
     public void movimentoPersonagem(Jogador jogador, Mapa mapa){
-        Scanner scanf = new Scanner(System.in);
-        char comando;
-        int posicaoX, posicaoY;
-        
-        int rodando = 1;
-        while (rodando != 0) {
-            IO.println("Use WASD para movimentar:");
-            comando = scanf.next().charAt(0);
-            scanf.nextLine();
-            posicaoY = jogador.getPosicaoy();
-            posicaoX = jogador.getPosicaox();
-            int antigoX = posicaoX;
-            int antigoY = posicaoY;
-            int teste;
-            if(comando == 'w'){
-                teste = posicaoY - 1;
-                if (teste != null) {
-                    
+        try (Scanner scanf = new Scanner(System.in)){
+            char comando;
+            int rodando = 1;
+
+            while (rodando != 0) {
+                if (jogador.getKitsMedicos() > 0) {
+                    IO.println("Use WASD para mover, C para curar (ou 0 para sair):");
+                } else {
+                    IO.println("Use WASD para movimentar (ou 0 para sair):");
                 }
-                posicaoY = antigoY - 1;
-            }else if (comando == 's') {
-                posicaoY = antigoY + 1;
-            }else if (comando == 'a') {
-                posicaoX = antigoX - 1;
-            }else if (comando == 'd') {
-                posicaoX = antigoX+1;
+                comando = scanf.next().charAt(0);
+                scanf.nextLine();
+
+                if (comando == '0') {
+                    rodando = 0;
+                    continue; 
+                }
+
+                int proximoX = jogador.getPosicaox();
+                int proximoY = jogador.getPosicaoy();
+
+                switch (comando) {
+                    case 'w'-> { proximoX = proximoX - 1; }
+                    case 's'-> { proximoX = proximoX + 1; }
+                    case 'a'-> { proximoY = proximoY - 1; }
+                    case 'd'-> { proximoY = proximoY + 1; }
+                    case 'c'-> {
+                        if (jogador.getKitsMedicos() > 0) {
+                            if (jogador.getSaude() < 5) {
+                                jogador.setSaude(5);
+                                jogador.setKitsMedicos(jogador.getKitsMedicos() - 1);
+                                IO.println("Você usou um Kit Médico e restaurou sua vida!");
+                            } else {
+                                IO.println("Sua vida já está cheia!");
+                            }
+                        } else {
+                            IO.println("Você não possui Kits Médicos no inventário!");
+                        }
+                    }
+                    default-> { IO.println("Comando inválido!"); }
+                }
+
+                if (comando != 'c') {
+                    if (proximoX >= 0 && proximoX < tamanho && proximoY >= 0 && proximoY < tamanho) {
+                        
+                       if (!grade[proximoX][proximoY].isParede()) {
+                        int xAntigo = jogador.getPosicaox();
+                        int yAntigo = jogador.getPosicaoy();
+                        
+                            if (grade[proximoX][proximoY].getDinossauro() != null) {
+                                Dinossauro inimigo = grade[proximoX][proximoY].getDinossauro();
+                                boolean vitoria = iniciarCombate(jogador, inimigo, scanf);
+                            
+                                if (vitoria) {
+                                 grade[proximoX][proximoY].setDinossauro(null);
+                                
+                                    grade[xAntigo][yAntigo].setJogador(null);
+                                    jogador.setPosicaox(proximoX);
+                                    jogador.setPosicaoy(proximoY);
+                                    grade[proximoX][proximoY].setJogador(jogador);
+                                }   
+                            
+                            } else {
+                                grade[xAntigo][yAntigo].setJogador(null);
+                                
+                                if (grade[proximoX][proximoY].getCaixa() != null) {
+                                    CaixaSuprimentos caixa = grade[proximoX][proximoY].getCaixa();
+                                    
+                                    if (caixa.getDinossauroSurpresa() != null) {
+                                        IO.println("SURPRESA! Um Compsognato pulou da caixa!");
+                                        int dadoPercepcao = ThreadLocalRandom.current().nextInt(1, 4); 
+                                        if (dadoPercepcao <= jogador.getPercepcao()) {
+                                            IO.println("[ESQUIVA!] Graças à sua percepção, você se esquivou do bote!"); 
+                                        } else {
+                                            jogador.setSaude(jogador.getSaude() - 1); 
+                                            IO.println("[DANO!] Você não reagiu a tempo e perdeu 1 ponto de vida!");
+                                        }
+                                    } else if (caixa.getItemGuardado() instanceof KitMedico) {
+                                        jogador.setKitsMedicos(jogador.getKitsMedicos() + 1);
+                                        IO.println("Você encontrou um Kit Médico!");
+                                    } else if (caixa.getItemGuardado() instanceof BastaoEletrico) {
+                                        jogador.setTemBastao(true);
+                                        IO.println("Você encontrou um Bastão Elétrico!");
+                                    } else if (caixa.getItemGuardado() instanceof ArmaDeDardos) {
+                                        jogador.setTemArma(true);
+                                        jogador.setMunicao(jogador.getMunicao() + 1); 
+                                        IO.println("Você encontrou uma Arma de Dardos (+1 Munição)!");
+                                    }
+                                    
+                                    grade[proximoX][proximoY].setCaixa(null);
+                                }
+                                
+                                jogador.setPosicaox(proximoX);
+                                jogador.setPosicaoy(proximoY);
+                                grade[proximoX][proximoY].setJogador(jogador);
+                            }
+                        }
+                    }
+                }
+                mapa.movimentarDinossauros(jogador, scanf);
+                
+                if (jogador.getSaude() <= 0) {
+                    IO.println("\n GAME OVER! Você não aguentou os ferimentos e morreu!!");
+                    rodando = 0;
+                }else{
+                    mapa.ImprimirMapa();
+                    mapa.exibirStatusJogador(jogador);
+                }  
             }
-            grade[antigoX][antigoY].setJogador(null);
-            jogador.setPosicaox(posicaoX);
-            jogador.setPosicaoy(posicaoY);
-            grade[posicaoX][posicaoX].setJogador(jogador);
-            mapa.ImprimirMapa();
-            
         }
-        scanf.close();
+    }
+    public void exibirStatusJogador(Jogador jogador) {
+    IO.println("--- STATUS ---");
+    
+    // Puxa a vida atual do jogador
+    IO.println("Vida: " + jogador.getSaude());
+    
+    if (jogador.isTemArma()) {
+        IO.println("Arma: Equipada");
+        IO.println("Munição: " + jogador.getMunicao());
+    } else {
+        IO.println("Arma: Nenhuma");
+    }
+    
+    IO.println("Kits Médicos: " + jogador.getKitsMedicos());
+    
+    IO.println("--------------");
+}
+    public boolean iniciarCombate(Jogador jogador, Dinossauro inimigo, Scanner scanf) {
+    IO.println("\nCOMBATE INICIADO! ");
+    
+        while (jogador.getSaude() > 0 && inimigo.getSaude() > 0) {
+            IO.println("\nVida do Jogador: " + jogador.getSaude() + " HP | Vida do Inimigo: " + inimigo.getSaude() + " HP");
+            IO.println("Escolha sua ação:");
+            IO.println("1 - Atacar");
+            IO.println("2 - Fugir");
+            
+            int acao = scanf.nextInt();
+            scanf.nextLine();
+            
+            if (acao == 2) {
+                IO.println("Você conseguiu fugir do combate!");
+                return false; 
+            }
+            
+            if (acao == 1) {
+                IO.println("Como você deseja desferir o ataque?");
+                if (jogador.isTemBastao()) {
+                    IO.println("1 - Bastão Elétrico");
+                } else {
+                    IO.println("1 - Mãos Nuas");
+                }
+                if (jogador.isTemArma()) {
+                    IO.println("2 - Arma de Dardos Tranquilizantes (Munição: " + jogador.getMunicao() + ")");
+                }
+                
+                int tipoAtaque = scanf.nextInt();
+                scanf.nextLine();
+                
+                int danoCausado = 0;
+                
+                if (tipoAtaque == 2 && jogador.isTemArma()) {
+                    if (jogador.getMunicao() > 0) {
+                        jogador.setMunicao(jogador.getMunicao() - 1); 
+                        
+                        if (inimigo instanceof Velociraptor) {
+                            IO.println("O Velociraptor é muito ágil! Ele desviou do dardo tranquilizante facilmente!");
+                            danoCausado = 0;
+                        } else {
+                            IO.println(" Tiro certeiro! O dardo aplicou um golpe crítico de 2 pontos de dano!"); 
+                            danoCausado = 2;
+                        }
+                    } else {
+                        IO.println("Você tentou puxar o gatilho, mas está sem munição! Acabou atacando sem querer com o que tinha.");
+                        tipoAtaque = 1; 
+                    }
+                }
+                
+                if (tipoAtaque == 1) {
+                    int dadoAtaque = ThreadLocalRandom.current().nextInt(1, 7); 
+                    IO.println("Você rolou o dado de ataque e tirou: " + dadoAtaque);
+                    
+                    if (inimigo instanceof TiranossauroRex && !jogador.isTemBastao()) {
+                        IO.println("Seus socos não fazem efeito na pele grossa do T-Rex! Você precisa de uma arma!"); 
+                        danoCausado = 0;
+                    } else if (jogador.isTemBastao()) {
+                        if (dadoAtaque > 5) {
+                            IO.println("Choque Crítico! O bastão descarregou alta voltagem (+2 de dano)!");
+                            danoCausado = 2;
+                        } else if (dadoAtaque == 1) {
+                            IO.println("Você errou o balanço do bastão elétrico e não causou dano!");
+                            danoCausado = 0;
+                        } else {
+                            IO.println("O bastão elétrico atingiu o dinossauro (+1 de dano)!");
+                            danoCausado = 1;
+                        }
+                    } else {
+                        if (dadoAtaque == 6) {
+                            IO.println("Golpe Crítico! Você acertou um ponto fraco com as mãos nuas (+2 de dano)!");
+                            danoCausado = 2;
+                        } else if (dadoAtaque == 1 || dadoAtaque == 2) {
+                            IO.println("Você socou o ar e não causou dano!");
+                            danoCausado = 0;
+                        } else {
+                            IO.println("Soco conectado com sucesso (+1 de dano)!");
+                            danoCausado = 1;
+                        }
+                    }
+                }
+                
+                inimigo.setSaude(inimigo.getSaude() - danoCausado);
+                
+                if (inimigo.getSaude() > 0) {
+                    IO.println("🦖 O dinossauro avança para morder você!");
+                    int dadoDefesa = ThreadLocalRandom.current().nextInt(1, 4); // Dado de 3 lados [cite: 107]
+                    
+                    if (dadoDefesa <= jogador.getPercepcao()) {
+                        IO.println("[ESQUIVA!] Seus reflexos baseados na percepção fizeram você desviar do bote!"); // [cite: 108]
+                    } else {
+                        int danoSofrido = (inimigo instanceof TiranossauroRex) ? 2 : 1;
+                        jogador.setSaude(jogador.getSaude() - danoSofrido);
+                        IO.println("[DANO!] Você foi atingido e perdeu " + danoSofrido + " ponto(s) de vida!"); // [cite: 109]
+                    }
+                }
+            }
+        }
+    
+        return inimigo.getSaude() <= 0;
+    }
+    public void movimentarDinossauros(Jogador jogador, Scanner scanf) {//se eu não passar o scanf por parametro, no segundo combate o scanf que eu criar novamente dentro do novo método será destruido autoaticamente, e dará erro
+        ArrayList<Dinossauro> dinossaurosParaMover = new ArrayList<>();
+        
+        for (int i = 0; i < tamanho; i++) {
+            for (int j = 0; j < tamanho; j++) {
+                Dinossauro dino = grade[i][j].getDinossauro();
+                if (dino != null && !(dino instanceof TiranossauroRex)) {
+                    dinossaurosParaMover.add(dino);
+                }
+            }
+        }
+        
+        for (Dinossauro dino : dinossaurosParaMover) {
+            // Pega as posições atuais (Assumindo que Dinossauro herda getPosicaox/y de Personagem)
+            int xAtual = dino.getPosicaox();
+            int yAtual = dino.getPosicaoy();
+            int novoX = xAtual;
+            int novoY = yAtual;
+            
+            int direcao = ThreadLocalRandom.current().nextInt(0, 4);
+            switch(direcao) {
+                case 0 -> novoX--;
+                case 1 -> novoX++;
+                case 2 -> novoY--;
+                case 3 -> novoY++;
+            }
+            
+            if (novoX >= 0 && novoX < tamanho && novoY >= 0 && novoY < tamanho) {
+                
+                if (!grade[novoX][novoY].isParede() && grade[novoX][novoY].getDinossauro() == null) {
+                    
+                    grade[xAtual][yAtual].setDinossauro(null);
+                    
+                    dino.setPosicaox(novoX);
+                    dino.setPosicaoy(novoY);
+                    
+                    grade[novoX][novoY].setDinossauro(dino);
+                    
+                    if (novoX == jogador.getPosicaox() && novoY == jogador.getPosicaoy()) {
+                        IO.println("\n ALERTA: Um " + dino.getClass().getSimpleName() + " encontrou você!");
+                        
+                        boolean vitoria = iniciarCombate(jogador, dino, scanf);
+                        
+                        if (vitoria) {
+                            grade[novoX][novoY].setDinossauro(null);
+                        } else if (jogador.getSaude() <= 0) {
+                            return; 
+                        }
+                    }
+                }
+            }
+        }
     }
 }
